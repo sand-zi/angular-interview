0e16a478b94290e93b18e61110daa4de4b021580
Sandra Zufman <zinkeen@gmail.com>
2021-06-22 08:42:15 +0300

    Let's go

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppHeaderComponent } from './cmps/app-header/app-header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: AppHeaderComponent
+  // }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
